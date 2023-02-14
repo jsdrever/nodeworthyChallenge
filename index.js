ecd14946126162aa2ebb4674776c03a96f3ae64d
jsdrever <jsdrever09@gmail.com>
2023-02-13 18:14:12 -0600

    baseline

---
+++ index.js
@@ -0,0 +1,16 @@
+const generateMarkdown = require("./utils/generateMarkdown");
+
+// TODO: Include packages needed for this application
+module.import = generateMarkdown
+// TODO: Create an array of questions for user input
+//!title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
